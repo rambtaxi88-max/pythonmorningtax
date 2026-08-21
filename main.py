a7a17bb49e824414673cbdc412b01a784388748b
rambtaxi88-max <ramb.taxi88@gmail.com>
2026-08-21 10:30:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:4200",
-        "http://127.0.0.1:4200"
+        "http://127.0.0.1:4200",
+        "http://187.52.119.33:8000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
